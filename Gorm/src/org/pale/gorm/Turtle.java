c22483bfeef26846b98ce7918d5ea438e0171f07
Jim Finnis <jim.finnis@gmail.com>
2013-09-09 21:52:16 +0100

    Great, but all the directions for the turtle are reversed.

--- Gorm/src/org/pale/gorm/Turtle.java
+++ Gorm/src/org/pale/gorm/Turtle.java
@@ -26,8 +26,21 @@ public class Turtle {
 
 	private int mode = 0;
 	private boolean aborted;
-	private int loopPoint=-1;
-	static final int FOLLOW = 1; // wall following mode
+	private int loopPoint = -1;
+
+	private boolean isModeFlag(int f) {
+		return (mode & f) != 0;
+	}
+
+	// flags
+	static final int FOLLOW = 1; // follow walls if we hit them
+	static final int SUPPORTFOLLOW = 2; // edge following on (build direction
+										// will turn to ensure support under
+										// built blocks)
+	static final int LEFT = 4; // following will try left turn first
+	static final int RANDOM = 8; // following will try either left or right
+									// (overrides LEFT)
+	static final int HUGEDGE = 16;	// we must always keep a wall to our left or right, and follow them round
 
 	char getNext() {
 		if (cur >= string.length())
@@ -49,15 +62,16 @@ public class Turtle {
 		char c;
 		string = s;
 		aborted = false;
-		for(;;){
+		for (;;) {
 			c = getNext();
-			if(c==0){
-				if(loopPoint>=0){
+			if (c == 0) {
+				if (loopPoint >= 0) {
 					cur = loopPoint;
 					c = getNext();
-				}
+				}else return;
 			}
-			if(aborted)return;
+			if (aborted)
+				return;
 			execute(c);
 		}
 	}
@@ -66,8 +80,10 @@ public class Turtle {
 		switch (c) {
 		case 'f': // forward
 			pos = pos.add(dir);
-			if ((mode & FOLLOW) != 0)
+			if (isModeFlag(FOLLOW))
 				follow();
+			else if (isModeFlag(HUGEDGE))
+				hugEdge();
 			break;
 		case 'r': // turn right
 			dir = dir.rotate(1);
@@ -81,8 +97,9 @@ public class Turtle {
 		case 'd':// go up
 			pos = pos.add(0, -1, 0);
 			break;
-		case 'w': // write the material to the current block - but abort if we are going to overwrite!
-			if(isEmpty(0,0,0))
+		case 'w': // write the material to the current block - but abort if we
+					// are going to overwrite!
+			if (isEmpty(0, 0, 0))
 				write();
 			else
 				abort();
@@ -101,112 +118,18 @@ public class Turtle {
 		}
 	}
 
-	/**
-	 * If the left and right of this square are both empty, turn to follow the
-	 * wall around. This follow mode allows us to follow outside edges.
-	 */
-	private void follow() {
-		GormPlugin.log("following: dir "+dir.toString());
-		dumpMap();
-		if (isEmpty(-1,0,0) && isEmpty(1,0,0)) {
-			GormPlugin.log("left and right are empty");
-			if (r.nextBoolean()) {
-				// attempt follow left first
-				if (!followLeft())
-					if (!followRight())
-						abort();
-			} else {
-				// attempt follow left first
-				if (!followRight())
-					if (!followLeft())
-						abort();
-			}
-		}
-
-	}
-
-	/**
-	 * Look at the block relative to the turtle - in turtle space - and tell whether it's empty
-	 * @param x
-	 * @param z
-	 * @return
-	 */
-	private boolean isEmpty(int x,int y,int z) {
-		IntVector v = new IntVector(x,y,z).rotateToSpace(dir);
-		IntVector p = pos.add(v);
-		Block b = world.getBlockAt(p.x, p.y, p.z);
-		//GormPlugin.log("  block at "+v.toString()+" is "+b.getType().toString());
-
-		return b.isEmpty();
-	}
-
-	/**
-	 * End the run
-	 */
-	private void abort() {
-		dumpMap();
-		aborted = true;
-	}
-
-	/**
-	 * Useful debugging; print 5x5 map relative to the turtle = X is solid, _ is empty.
-	 */
-	private void dumpMap() {
-		for(int z=2;z>=-2;z--){
-			String foo=String.format("%3d",z);
-			for(int x=-2;x<=2;x++){
-				IntVector v = new IntVector(x,0,z).rotateToSpace(dir);
-				IntVector p = pos.add(v);
-				Block b = world.getBlockAt(p.x, p.y, p.z);
-				if(b.getType()==Material.LAPIS_BLOCK)
-					foo+="L";
-				else if(isEmpty(x,0,z))
-					foo+="_";
-				else
-					foo+="X";
-			}
-			GormPlugin.log("MAP: "+foo);
-		}
-	}
-
-	/**
-	 * With nothing on the left or the right, attempt to follow the left hand
-	 * wall we just left.
-	 * 
-	 * @return
-	 */
-	private boolean followLeft() {
-		if(isEmpty(-1,0,-1)){ // if the spot behind and to the left isn't a wall, fail
-			GormPlugin.log("follow left failed");
-			return false;
-		} else {
-			dir = dir.rotate(3); // or turn left
-			GormPlugin.log("follow left OK");
-			return true;
-		}
-	}
-
-	/**
-	 * With nothing on the left or the right, attempt to follow the right hand
-	 * wall we just left.
-	 * 
-	 * @return
-	 */
-	private boolean followRight() {
-		if(isEmpty(1,0,-1)){ // if the spot behind and to the right isn't a wall, fail
-			GormPlugin.log("follow right failed");
-			return false;
-		} else {
-			dir = dir.rotate(1); // or turn right
-			GormPlugin.log("follow right OK");
-			return true;
-		}
-	}
-
 	private int getNextMode() {
 		switch (getNext()) {
 		case 'f':
 			return FOLLOW;
+		case 's':
+			return SUPPORTFOLLOW;
+		case 'l':
+			return LEFT;
+		case '?':
+			return RANDOM;
+		case 'h':
+			return HUGEDGE;
 		}
 		return 0;
 	}
@@ -232,6 +155,181 @@ public class Turtle {
 			break;
 		}
 
+	}
+
+	/**
+	 * Return true if we try to turn left first when automatically turning
+	 * 
+	 * @return
+	 */
+	private boolean tryLeftFirst() {
+		// if we are in random mode, it depends on a random number which way we
+		// try first,
+		// otherwise it depends on the LEFT flag.
+		return (!isModeFlag(RANDOM) && isModeFlag(LEFT) || isModeFlag(RANDOM)
+				&& r.nextBoolean());
+
+	}
+
+	/**
+	 * If we're about to hit a wall, turn to avoid it
+	 */
+	private void follow() {
+		GormPlugin.log("following: dir " + dir.toString());
+
+		if (!isEmpty(0, 0, 1)) { // ahead is full, we're going to hit a wall -
+									// attempt INNER FOLLOW
+			if (tryLeftFirst()) {
+				// attempt follow left first
+				if (!followInnerLeft())
+					if (!followInnerRight())
+						abort();
+			} else {
+				// attempt follow left first
+				if (!followInnerRight())
+					if (!followInnerLeft())
+						abort();
+			}
+		}
+	}
+	
+	/**
+	 * If the left and right of this square are both empty, turn to follow the
+	 * wall around. This follow mode allows us to follow outside edges.
+	 */
+	private void hugEdge(){
+		if (isEmpty(-1, 0, 0) && isEmpty(1, 0, 0)) { // left and right are empty
+														// - attempt OUTER
+														// FOLLOW
+														// (i.e. following the
+														// outer edge)
+			if (tryLeftFirst()) {
+				// attempt follow left first
+				if (!followOuterLeft())
+					if (!followOuterRight())
+						abort();
+			} else {
+				// attempt follow left first
+				if (!followOuterRight())
+					if (!followOuterLeft())
+						abort();
+			}
+		}
+	}
+
+	/**
+	 * With nothing on the left or the right, attempt to follow the left hand
+	 * wall we just left.
+	 * 
+	 * @return
+	 */
+	private boolean followOuterLeft() {
+		if (isEmpty(-1, 0, -1)) { // if the spot behind and to the left isn't a
+									// wall, fail
+			GormPlugin.log("follow left failed");
+			return false;
+		} else {
+			dir = dir.rotate(3); // or turn left
+			GormPlugin.log("follow left OK");
+			return true;
+		}
+	}
+
+	/**
+	 * With nothing on the left or the right, attempt to follow the right hand
+	 * wall we just left.
+	 * 
+	 * @return
+	 */
+	private boolean followOuterRight() {
+		if (isEmpty(1, 0, -1)) { // if the spot behind and to the right isn't a
+									// wall, fail
+			GormPlugin.log("follow right failed");
+			return false;
+		} else {
+			dir = dir.rotate(1); // or turn right
+			GormPlugin.log("follow right OK");
+			return true;
+		}
+	}
+
+	/**
+	 * We're one step in front of a wall - we need to turn to avoid it
+	 */
+	private boolean followInnerLeft() {
+		if (isEmpty(-1, 0, 0)) {
+			GormPlugin.log("inner follow left OK");
+			dir = dir.rotate(3);
+			return true;
+		} else {
+			GormPlugin.log("inner follow left failed");
+			dumpMap();
+			return false;
+		}
+	}
+
+	/**
+	 * We're one step in front of a wall - we need to turn to avoid it
+	 */
+	private boolean followInnerRight() {
+		if (isEmpty(1, 0, 0)) {
+			GormPlugin.log("inner follow right OK");
+			dir = dir.rotate(1);
+			return true;
+		} else {
+			GormPlugin.log("inner follow right failed");
+			dumpMap();
+			return false;
+		}
+
+	}
+
+	/**
+	 * Look at the block relative to the turtle - in turtle space - and tell
+	 * whether it's empty
+	 * 
+	 * @param x
+	 * @param z
+	 * @return
+	 */
+	private boolean isEmpty(int x, int y, int z) {
+		IntVector v = new IntVector(x, y, z).rotateToSpace(dir);
+		IntVector p = pos.add(v);
+		Block b = world.getBlockAt(p.x, p.y, p.z);
+		// GormPlugin.log("  block at "+v.toString()+" is "+b.getType().toString());
+
+		return b.isEmpty();
+	}
+
+	/**
+	 * End the run
+	 */
+	private void abort() {
+		GormPlugin.log("aborted");
+		dumpMap();
+		aborted = true;
+	}
+
+	/**
+	 * Useful debugging; print 5x5 map relative to the turtle = X is solid, _ is
+	 * empty.
+	 */
+	private void dumpMap() {
+		for (int z = 2; z >= -2; z--) {
+			String foo = String.format("%3d", z);
+			for (int x = -2; x <= 2; x++) {
+				IntVector v = new IntVector(x, 0, z).rotateToSpace(dir);
+				IntVector p = pos.add(v);
+				Block b = world.getBlockAt(p.x, p.y, p.z);
+				if (b.getType() == Material.LAPIS_BLOCK)
+					foo += "L";
+				else if (isEmpty(x, 0, z))
+					foo += "_";
+				else
+					foo += "X";
+			}
+			GormPlugin.log("MAP: " + foo);
+		}
 	}
 
 	private Block get() {

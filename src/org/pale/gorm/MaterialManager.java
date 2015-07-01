6d4235ae66024807a96ed52395645c97120d3d31
Jim Finnis <jim.finnis@gmail.com>
2015-07-01 21:54:47 +0100

    Pattern matching in material set names for biomes. Commented config.yml.

--- src/org/pale/gorm/MaterialManager.java
+++ src/org/pale/gorm/MaterialManager.java
@@ -1,5 +1,6 @@
 package org.pale.gorm;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -8,6 +9,8 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.bukkit.Material;
 import org.bukkit.block.Biome;
@@ -159,19 +162,61 @@ public class MaterialManager {
 		}
 		// and the default
 		loadMatMetaListForBiome(null);
+		List<String> defaultMetaListNames = biomeMetaListNames.get(null);
+		if(defaultMetaListNames==null)
+			log.severe("no default metalist given - could cause problems!");
+
+		// first build a list of patterns and the matlists to which they resolve.
+		ConfigurationSection sec = conf.getConfigurationSection("matmatch");
+		List<Map.Entry<Pattern,List<String>>> patternList = new ArrayList<Map.Entry<Pattern,List<String>>>();
+		for(Map.Entry<String, Object> e: sec.getValues(false).entrySet()){
+			String regex = e.getKey();
+			// now because YAML is rubbish we have to replace some things in the regex
+			regex = regex.replace("*", ".*");
+			log.info("New matcher entry: "+regex);
+			Pattern p = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+			if(e.getValue() instanceof List<?>){
+				Map.Entry<Pattern, List<String>> ne = 	
+						new AbstractMap.SimpleEntry<Pattern, List<String>>(p,(List<String>)e.getValue());
+				for(String ss: ne.getValue()){
+					log.info("  New val: "+ss);
+				}
+				patternList.add(ne);
+			} else throw new RuntimeException("value not a string list in matmatch block");
+		}
+
+		// now go through the biome list and see if we can find a match for those with no matlist already
+		// set. There are, no doubt, more efficient ways.
+
+		for(Biome b: Biome.values()){
+			String s = b.toString();
+			List<String> res = null;
+			for(Map.Entry<Pattern,List<String>> e: patternList){
+				Matcher m = e.getKey().matcher(s);
+				if(m.matches()){
+					res = e.getValue();break;
+				}
+			}
+			if(res!=null){
+				log.info("match found for "+s+", maps to:"+res);
+				biomeMetaListNames.put(b, res);
+			} else {
+				biomeMetaListNames.put(b, defaultMetaListNames);
+			}
+		}
+
+
 		// load the actual matlists
-		ConfigurationSection sec = conf.getConfigurationSection("mats");
+		sec = conf.getConfigurationSection("mats");
 		for(String s:sec.getKeys(false)){
 			MaterialDataPair[][] list = getMats("mats."+s); // getMats uses the whole file namespace.
 			log.info("Retrieved matlist "+s+", size: "+(list==null?"null":list.length));
 			materialLists.put("mats."+s, list);
-//			log.info("Matlist stored as mats."+s+":");
-//			for(int i=0;i<MATLISTCT;i++){
-//				log.info(" list ["+i+"] :"+(list[i]==null?"null":list[i].length));
-//			}
+			//			log.info("Matlist stored as mats."+s+":");
+			//			for(int i=0;i<MATLISTCT;i++){
+			//				log.info(" list ["+i+"] :"+(list[i]==null?"null":list[i].length));
+			//			}
 		}
-
-
 
 		// we now have the biome meta lists, which are lists of names of material lists.
 		// Now we can dereference those. But first we resolve the default (null key)
@@ -212,13 +257,13 @@ public class MaterialManager {
 					log.severe("Base material list not found:"+baseName);
 				if(matList==null)
 					log.severe("Actual material list not found (shouldn't happen!):"+name);
-//				for(int i=0;i<MATLISTCT;i++){
-//					log.info("Baselist ["+i+"] :"+(baseList[i]==null?"null":baseList[i].length));
-//				}
+				//				for(int i=0;i<MATLISTCT;i++){
+				//					log.info("Baselist ["+i+"] :"+(baseList[i]==null?"null":baseList[i].length));
+				//				}
 				for(int i=0;i<MATLISTCT;i++){
 					if(matList[i]==null || matList[i].length==0){
 						repeat=true;
-//						log.info("No material list found for "+name+":"+i+", attempting base deref to "+baseName+":"+i);
+						//						log.info("No material list found for "+name+":"+i+", attempting base deref to "+baseName+":"+i);
 						matList[i] = baseList[i];
 					}
 				}
@@ -236,17 +281,11 @@ public class MaterialManager {
 		List<String> lst = GormPlugin.getInstance().getConfig()
 				.getStringList("matlist."+name);
 		if(lst==null || lst.size()==0){
-			log.info("No metalist for biome: "+name+", using default");
-			lst = GormPlugin.getInstance().getConfig().getStringList("matlist.default");
+			log.info("No metalist for biome: "+name+", using default or match");
 		}else{
 			log.info("Loaded metalist for biome: "+name+", count = "+String.valueOf(lst.size()));			
 		}
-		if(lst==null || lst.size()==0){
-			log.severe("no metalist for "+name+" and no default either!");
-		} else {
-			// tell the biome what the names of the lists are
-			biomeMetaListNames.put(biome, lst);
-		}
+		biomeMetaListNames.put(biome, lst);
 	}
 
 
